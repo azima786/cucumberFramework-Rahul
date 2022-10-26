bd9f937da1253ab5cb5aa1829906e6058c1c3d5e
Azima Keshwani <[vazima786@gmail.comgit config --global user.email [azima786@gmail.com]>
2022-10-26 16:29:00 -0500

    update

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features="src/test/java/features",
-        glue = "stepDefinitions", monochrome = true)
+        glue = "stepDefinitions", monochrome = true, plugin = {"html:target/cucumber.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
     @DataProvider(parallel = true)
